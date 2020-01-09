b1680802beae5c0231cf27fc967abb764b57a8ad
Yuki <yuki.dees39th@gmail.com>
2020-01-09 20:30:06 +0900

    センターどめのサンプル

---
+++ nuxt-firebase/components/Center.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <progress value="0" max="100" id="uploader">0%</progress>
+    <input type="file" value="upload" id="fileButton" />
+  </div>
+</template>
+<style>
+body {
+  display: flex;
+  min-height: 100vh;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+#uploader {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 50%;
+  margin-bottom: 10px;
+}
+</style>
