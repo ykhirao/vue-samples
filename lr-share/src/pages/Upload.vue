43c5897f7e3b40b607e5cab9fc9def8c9fcc5b21
Yuki <yuki.dees39th@gmail.com>
2020-01-11 17:23:53 +0900

    update

---
+++ lr-share/src/pages/Upload.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>{{ name }} ({{ age }})</div>
+    <div>{{ triple }}</div>
+    <div>
+      <button @click="onClickButton">Click Me!</button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {},
+  directives: {},
+  filters: {},
+  mixins: []
+})
+export default class Update extends Vue {
+  @Prop() userName: string = 'tanaka'
+
+  name = 'simochee'
+  age = 21
+  get triple() {
+    return this.age * 3
+  }
+  onClickButton() {
+    console.log('fire')
+  }
+  mounted() {
+    // コンポーネントがマウントされたときの処理
+  }
+}
+</script>
