5968499dfc126470182c9e4ae803dde96ea3b48e
Yuki <yuki.dees39th@gmail.com>
2020-01-19 11:04:52 +0900

    update

---
+++ animate-vue/src/pages/upload.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>This is an upload page</h1>
+    <chartist
+      ratio="ct-major-second"
+      type="Line"
+      :data="chartData"
+      :options="chartOptions"
+    >
+    </chartist>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Signin extends Vue {
+  email: string = ''
+  chartData: object = {
+    labels: ['A', 'B', 'C'],
+    series: [
+      [1, 3, 2],
+      [4, 6, 2]
+    ]
+  }
+  chartOptions: object = {
+    lineSmooth: true
+  }
+}
+</script>
+<style scoped>
+@import '../../node_modules/chartist/dist/chartist.min.css';
+</style>
--- animate-vue/src/router.ts
+++ animate-vue/src/router.ts
@@ -4,12 +4,14 @@ import routes from 'vue-auto-routing'
 import { createRouterLayout } from 'vue-router-layout'
 
 Vue.use(Router)
+Vue.use(require('vue-chartist'))
 
 const RouterLayout = createRouterLayout(layout => {
   return import('@/layouts/' + layout + '.vue')
 })
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
