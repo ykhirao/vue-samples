da4036a5af69f463aa9cf87feedda1d7c1da79c5
Yuki <yuki.dees39th@gmail.com>
2020-02-25 01:41:43 +0900

    update

--- react-1/src/index.js
+++ react-1/src/index.js
@@ -4,13 +4,36 @@ import './index.css'
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>
+    return (
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
+    )
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+    // this.state
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({ squares })
+  }
+
   renderSquare(i) {
-    return <Square value={i} />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
